247231da7fb0344ac32423ef93b688d8547da995
Krittika-Saha <krittika.saha.dev@gmail.com>
2021-02-12 14:33:06 +0530

    Upgraded code

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from requests import get
 from twilio.rest import Client
 OWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
-account_sid = "Your_sid_here"
-auth_token = 'Your_auth_token_here'
+account_sid = "_YOUR_SID_HERE"
+auth_token = '_YOUR_KEY_HERE_'
 weather_params = {
     "lat": "22.6288",
     "lon": "88.4311",
     "exclude": "current,daily,minutely",
-    "appid": "_Your_api_key_here",
+    "appid": "_YOUR_API_KEY_HERE",
     "hourly":"hourly.weather"
 }
 
 OWM_response = get(OWM_endpoint, params=weather_params)
 OWM_response.raise_for_status()
 weather_data = OWM_response.json()
-first_12_hours = [i for i in weather_data['hourly'] if weather_data['hourly'].index(i) <= 11]
+first_12_hours = weather_data['hourly'][:12]
 id = []
 for j in range(0, 12):
     for k in first_12_hours[j]['weather']:
@@ -24,8 +24,8 @@ for i in id:
         client = Client(account_sid, auth_token)
         message = client.messages.create(
             body="It's probably gonna rain, so bring an ☔",
-            from_='whatsapp:_Your_Number_Here_',
-            to='whatsapp:_Your_Number_Here_'
+            from_='whatsapp:_YOUR_TWILIO_PHONE_HERE_',
+            to='whatsapp:_YOUR_PHONE_HERE_'
         )
         print(message.status)
         break
